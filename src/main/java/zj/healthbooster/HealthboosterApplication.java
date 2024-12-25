95200c95c6b787bd5633a1b80855b8d4dbaf718a
Jalen Wu <jalenwu@jalens-air.mynetworksettings.com>
2024-12-24 20:47:12 -0500

    closes #1

--- src/main/java/zj/healthbooster/HealthboosterApplication.java
+++ src/main/java/zj/healthbooster/HealthboosterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HealthboosterApplication {
 
 	public static void main(String[] args) {
+		// example
 		SpringApplication.run(HealthboosterApplication.class, args);
 	}
 
