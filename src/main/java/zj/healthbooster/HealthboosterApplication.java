54d6a6494811b2bd110868055a31e7097c606b72
Jalen Wu <jalenwu@jalens-air.mynetworksettings.com>
2024-12-24 20:41:10 -0500

    Connected database to backend

---
+++ src/main/java/zj/healthbooster/HealthboosterApplication.java
@@ -0,0 +1,13 @@
+package zj.healthbooster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthboosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthboosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zj/healthbooster/HealthboosterApplicationTests.java
@@ -0,0 +1,13 @@
+package zj.healthbooster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthboosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
